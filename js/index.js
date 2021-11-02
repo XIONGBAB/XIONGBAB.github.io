1cc017fcd34dbd3c4fd7b672bb843287aa662600
XIONGBAB <85008817+XIONGBAB@users.noreply.github.com>
2021-11-02 19:25:37 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.all_box').hover(function () {
+        $('#rota').toggleClass('rota_box');
+        $('#rota').removeClass('rota_box2');
+        $('.content_box').stop().slideDown(500)
+    }, function () {
+        $('#rota').toggleClass("rota_box2")
+        $('#rota').removeClass("rota_box")
+        $('.content_box').stop().slideUp(500);
+    });
+})
