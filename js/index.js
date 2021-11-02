764bc157ef49910d8b5757b8ac7ac662e81fa485
XIONGBAB <85008817+XIONGBAB@users.noreply.github.com>
2021-11-02 19:52:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(function () {
         $('#rota').removeClass("rota_box")
         $('.content_box').stop().slideUp(500);
     });
-    $('.header').click(function () {
+    $('.header').tap(function () {
         $('.content_box').stop().slideUp(500);
     });
 })
