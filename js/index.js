751ece780b51721a99c29f7ac30af61842c81b4f
XIONGBAB <85008817+XIONGBAB@users.noreply.github.com>
2021-11-03 14:55:24 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,19 +3,20 @@ $(function () {
         $('#rota').toggleClass('rota_box');
         $('#rota').removeClass('rota_box2');
         $('.content_box').stop().slideDown(500);
-        // $('.logo').stop().fadeOut();
-        // $('#home').css('display', 'block');
     }, function () {
         $('#rota').toggleClass("rota_box2");
         $('#rota').removeClass("rota_box");
         $('.content_box').stop().slideUp(500);
-        // $('.logo').stop().fadeIn();
-        // $('#home').css('display', 'none');
     });
     $('#back').click(function () {
         $('.content_box').stop().slideUp(500);
     });
-
+    $('#back').click(function () {
+        $('.content_box').stop().slideUp(500);
+    });
+    $('.logo').click(function () {
+        $('.container_img').show();
+    });
     // click btn
     function hide_box(class_name) {
         $('.container_img').hide();
