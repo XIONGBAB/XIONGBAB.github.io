94d50452d67c11c2b036447f9bf0d44f40800f9b
XIONGBAB <85008817+XIONGBAB@users.noreply.github.com>
2022-01-12 11:11:49 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -113,9 +113,12 @@ window.addEventListener('load', function () {
         ul2.querySelector('.active').classList.remove('active');
         ul2.children[index].classList.add('active');
     });
-        document.onselectstart = function () {
-            return false;
-        };
+    node.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+    node.ontouchend = function () {
+        throw new Error("NO ERROR:禁止长按弹出的菜单");
+    }
 
     // 开启/关闭触摸滑动
     // var startX = 0;
